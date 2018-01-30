63666d6c4e81b31c2a67125aad02392119c7042d
kxyu_001 <369762419@qq.com>
2018-01-30 10:04:48 +0800

    add diabetes datas

---
+++ tc_diabetes/DataProcess_1.py
@@ -0,0 +1,7 @@
+
+import pandas as pd #数据分析
+import numpy as np #科学计算
+from pandas import Series,DataFrame
+
+data_train = pd.read_csv(r"C:\Users\ykx\PycharmProjects\machine-learning\tc_diabetes\datas\d_train_20180102.csv")
+data_train.columns
